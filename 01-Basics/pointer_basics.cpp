2f08561b114e80bae643133a991f91a6c2d67cfb
Arham <arhamibrahim870@gmail.com>
2026-08-27 23:24:00 +0500

    Basics of a pointer

---
+++ 01-Basics/pointer_basics.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=10;
+    cout<<"Val of x : "<<x<<endl;
+    cout<<"Address of x : "<<&x<<endl;
+
+    int *p=&x;       //pointer p storing the address of a variable x
+    cout<<"Val of p (storing x address): "<<p<<endl;
+    cout<<"Address of p : "<<&p<<endl;
+    cout<<"Val at the Address of p i.e dereference : "<<*p<<endl;
+
+    return 0;
+}
