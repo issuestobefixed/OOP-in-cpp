c4412597b45b7a43ca45dfb25883591bef131649
Arham <arhamibrahim870@gmail.com>
2026-09-04 00:37:58 +0500

    DMA for 2 2D arrays input and display their sum through an adder function

--- 01-Basics/DynamicMemoryAllocation.cpp
+++ 01-Basics/DynamicMemoryAllocation.cpp
@@ -1,5 +1,39 @@
 #include<iostream>
 using namespace std;
+void adder(int **arr1, int **arr2, int rows, int coloumns)
+{
+    int **arr3=new int *[rows];
+    for(int i=0;i<rows;i++)
+    {
+        arr3[i]=new int [coloumns];
+    }
+
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<coloumns;j++)
+        {
+            arr3[i][j]=arr1[i][j]+arr2[i][j];
+        }
+    }
+
+    cout<<"Final matrix after addition : "<<endl;
+
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<coloumns;j++)
+        {
+            cout<<arr3[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int i=0;i<rows;i++)
+    {
+        delete[] arr3[i];
+    }
+
+    delete[] arr3;
+}
 int main()
 {
     int rows=0, coloumns=0;
@@ -8,10 +42,37 @@ int main()
     cout<<"Enter coloumns of  matrix : ";
     cin>>coloumns;
 
-    int **arr=new int *[rows];
+    int **arr1=new int *[rows];
     for(int i=0;i<rows;i++)
     {
-        arr[i]=new int [coloumns];
+        arr1[i]=new int [coloumns];
+    }
+
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<coloumns;j++)
+        {
+            cout<<"Enter ["<<i<<"]["<<j<<"] of Matrix 1: ";
+            cin>>arr1[i][j];
+        }
+    }
+
+    cout<<endl;
+    cout<<"Matrix 1 : "<<endl;
+
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<coloumns;j++)
+        {
+            cout<<arr1[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    int **arr2=new int *[rows];
+    for(int i=0;i<rows;i++)
+    {
+        arr2[i]=new int [coloumns];
     }
 
     for(int i=0;i<rows;i++)
@@ -19,27 +80,38 @@ int main()
         for(int j=0;j<coloumns;j++)
         {
             cout<<"Enter ["<<i<<"]["<<j<<"] : ";
-            cin>>arr[i][j];
+            cin>>arr2[i][j];
         }
     }
 
     cout<<endl;
-    cout<<"Matrix : "<<endl;
+    cout<<"Matrix 2 : "<<endl;
 
     for(int i=0;i<rows;i++)
-    {
+    { 
         for(int j=0;j<coloumns;j++)
         {
-            cout<<arr[i][j];
+            cout<<arr2[i][j]<<" ";
         }
         cout<<endl;
     }
 
+    cout<<endl;
+    
+    adder(arr1,arr2,rows,coloumns);
+
     for(int i=0;i<rows;i++)
     {
-        delete[] arr[i];
+        delete[] arr1[i];
     }
 
-    delete[] arr;
+    delete[] arr1;
+
+    for(int i=0;i<rows;i++)
+    {
+        delete[] arr2[i];
+    }
+
+    delete[] arr2;
     return 0;
 }
