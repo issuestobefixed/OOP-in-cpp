8172e45d23fa4a9dcc91ed217bf7e913d2ea9a6b
Arham <arhamibrahim870@gmail.com>
2026-09-03 22:48:57 +0500

    Adding two arrays

--- 01-Basics/arrays_and_pointers.cpp
+++ 01-Basics/arrays_and_pointers.cpp
@@ -3,13 +3,25 @@ using namespace std;
 int main()
 {
     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int arr2[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     int *ptr=&arr[0][0];
-
+    int *ptr2=&arr2[0][0];
+    int arr3[3][3]={};
     for (int i=0;i<3;i++)
     {
         for (int j=0;j<3;j++)
         {
-            cout<<*(ptr+i*3+j)<<" ";
+            arr3[i][j]=*ptr+*ptr2;
+            ptr++;
+            ptr2++;
+        }
+        cout<<endl;
+    }
+    for (int i=0;i<3;i++)
+    {
+        for (int j=0;j<3;j++)
+        {
+            cout<<arr3[i][j]<<" ";
         }
         cout<<endl;
     }
