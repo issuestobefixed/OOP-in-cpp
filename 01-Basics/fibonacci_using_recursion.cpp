860eec18c0e8f6a240896a2a524b3a383faf98f0
Arham <arhamibrahim870@gmail.com>
2026-09-03 23:37:09 +0500

    Fibonacci series  using recrsion

---
+++ 01-Basics/fibonacci_using_recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int input)
+{
+    if(input==0)
+    {
+        return 0;
+    }
+    if(input==1)
+    {
+        return 1;
+    }
+    return fibonacci(input-1)+fibonacci(input-2);
+}
+int main()
+{
+    int input=0;
+    cout<<"Enter a number up to which you want to calculate the Fibonacci series : ";
+    cin>>input;
+    cout<<fibonacci(input)<<" ";
+    return 0;
+}
