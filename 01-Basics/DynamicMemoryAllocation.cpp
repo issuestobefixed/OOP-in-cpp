770bbdc796c59794cc827dc37e19100731b8e68e
Arham <arhamibrahim870@gmail.com>
2026-09-04 00:19:07 +0500

    DMA for 2D array input and display

--- 01-Basics/DynamicMemoryAllocation.cpp
+++ 01-Basics/DynamicMemoryAllocation.cpp
@@ -2,24 +2,42 @@
 using namespace std;
 int main()
 {
-    int size=0;
-    cout<<"Enter size of array : ";
-    cin>>size;
+    int rows=0, coloumns=0;
+    cout<<"Enter rows of matrix : ";
+    cin>>rows;
+    cout<<"Enter coloumns of  matrix : ";
+    cin>>coloumns;
 
-    int *arr=new int [size];
-
-    for(int i=0;i<size;i++)
+    int **arr=new int *[rows];
+    for(int i=0;i<rows;i++)
     {
-        cout<<"Enter "<<i<<" element : ";
-        cin>>arr[i];
+        arr[i]=new int [coloumns];
+    }
+
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<coloumns;j++)
+        {
+            cout<<"Enter ["<<i<<"]["<<j<<"] : ";
+            cin>>arr[i][j];
+        }
     }
 
     cout<<endl;
-    cout<<"Array : ";
+    cout<<"Matrix : "<<endl;
 
-    for(int i=0;i<size;i++)
+    for(int i=0;i<rows;i++)
     {
-        cout<<arr[i]<<" ";
+        for(int j=0;j<coloumns;j++)
+        {
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+
+    for(int i=0;i<rows;i++)
+    {
+        delete[] arr[i];
     }
 
     delete[] arr;
