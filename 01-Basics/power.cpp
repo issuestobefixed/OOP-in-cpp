d2f3017bfb11416fb43afa60f055548a4bfbca47
Arham <arhamibrahim870@gmail.com>
2026-09-06 23:32:41 +0500

    Calculating power using recursion

---
+++ 01-Basics/power.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int power(int x, int pow)
+{
+    if(pow==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return x*power(x, pow-1);
+    }
+}
+int main()
+{
+    int x=0, pow=0;
+    cout<<"Enter Number : ";
+    cin>>x;
+    cout<<"Enter Power : ";
+    cin>>pow;
+    cout<<"Result : "<<power(x, pow);
+}
