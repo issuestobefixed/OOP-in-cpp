afdbd53176a4802238c10cf660eaffa800af6b5c
Arham <arhamibrahim870@gmail.com>
2026-09-06 23:18:36 +0500

    printing 1->N using recursion

---
+++ 01-Basics/printing_numbers_using_recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void print(int x)
+{
+    if(x==0)
+    {
+        return;
+    }
+    else
+    {
+       print(x-1);
+       cout<<x<<" ";
+    }
+}
+int main()
+{
+    int x=0;
+    cout<<"Enter a number : ";
+    cin>>x;
+    print(x);
+    return 0;
+}
