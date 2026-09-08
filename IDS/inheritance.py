1f05592a06258e6449cdeece6032736d3f532b9f
Arham <arhamibrahim870@gmail.com>
2026-09-08 22:53:01 +0500

    Inheritance without super()

---
+++ IDS/inheritance.py
@@ -0,0 +1,14 @@
+class Animal:
+    
+    def talk(self):
+        print("I'm not human")
+         
+class Cat(Animal):
+    
+    def talk(self):
+        super().talk()
+        print("Meow")
+        
+        
+tom=Cat()
+tom.talk()
