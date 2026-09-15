f492b78d15487fabcd091e2fccdc3b0d019aa2fc
Arham <arhamibrahim870@gmail.com>
2026-09-15 23:09:04 +0500

    Linear search

---
+++ 00-Labs/Lab4-Task3.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target)
+{
+	if(n == 0)
+	{
+		return -1;
+	}
+	
+	cout << "Checking index " << n-1 <<endl ;
+	if(arr[n - 1] == target)
+	{
+		cout << "Target found at index : " << n-1 <<endl;
+		return n - 1;
+	}
+	return linearSearch(arr, n - 1, target);
+}
+
+int main()
+{
+	int arr[5] = {};
+
+	for (int i = 0;i < 5;i++)
+	{
+		cout << "Enter " << i << " : ";
+		cin >> arr[i];
+	}
+	cout << "Recieved Array : ";
+	for (int i = 0;i < 5;i++)
+	{
+		cout<< arr[i]<<" ";
+	}
+	cout << endl;
+	int target = 0;
+	cout << "Enter target : ";
+	cin >> target;
+
+	linearSearch(arr, 5, target);
+	return 0;
+}
