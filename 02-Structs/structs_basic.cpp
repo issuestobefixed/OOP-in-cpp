dbf6a793b008232207f2acc5520939f61e65a41d
Arham <arhamibrahim870@gmail.com>
2026-09-18 00:01:28 +0500

    using array of structs

--- 02-Structs/structs_basic.cpp
+++ 02-Structs/structs_basic.cpp
@@ -14,18 +14,25 @@ int main()
             cout<<"Name : "<<s.name<<endl;
             cout<<"GPA  : "<<s.GPA<<endl;
             cout<<"Age  : "<<s.age<<endl;
+            cout<<"============================"<<endl;
         }
     };
 
-    Student s1;
+    Student s[3];
 
-    cout<<"Enter Name : ";
-    cin>>s1.name;
-    cout<<"Enter GPA  : ";
-    cin>>s1.GPA;
-    cout<<"Enter Age  : ";
-    cin>>s1.age;
+    for(int i=0;i<3;i++)
+    {
+        cout<<"Enter "<<i<<" student's name : ";
+        cin>>s[i].name;
+        cout<<"Enter "<<i<<" student's GPA  : ";
+        cin>>s[i].GPA;
+        cout<<"Enter "<<i<<" student's age  : ";
+        cin>>s[i].age;
+    }
 
-    s1.print(s1);
+    s[0].print(s[0]);
+    s[0].print(s[1]);
+    s[0].print(s[2]);
+
     return 0;
 }
